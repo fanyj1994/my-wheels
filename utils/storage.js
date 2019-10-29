bb7777df1e1937a64dfced492e93b867e1e0f28c
fanyj1994 <946020997@qq.com>
2019-10-29 18:31:21 +0800

    update

--- utils/storage.js
+++ utils/storage.js
@@ -4,20 +4,15 @@
       if (!window.localStorage) {
         throw new Error('Your browser not support localStorage, please update your browser.')
       }
-  
       this.storage = window.localStorage || localStorage
     }
 
     get = (key) => this.storage.getItem(key)
-
     set = (key, value) => this.storage.setItem(key, value)
-
     remove = (...keys) => {
       keys.forEach(key => this.storage.removeItem(key))
     }
-
     clear = () => this.storage.clear()
-
     all = () => {
       let allKeys = []
       for (let i in this.storage) {
@@ -27,11 +22,8 @@
       }
       return allKeys
     }
-
     has = (item) => this.all().indexOf(item) > -1 ? true : false
-
   }
-  
   const storage = new Storage()
   window.storage = storage
 })()
